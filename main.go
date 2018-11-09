8f86962910c1ab9b023767f809d2a63174e80622
Josh Walker <josh.walker@returnpath.com>
2018-11-09 13:06:47 -0700

    Add initial repl package

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/Interpreter/repl"
+)
+
 func main() {
 
+	currentUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Initializing command prompt for %s\n", currentUser.Username)
+	repl.Start(os.Stdin, os.Stdout)
 }
